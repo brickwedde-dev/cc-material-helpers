38eb36000b593164a985241d0dd83f49c302e188
Alex Brickwedde <alex@brickwedde.de>
2020-10-20 08:16:38 +0200

    Added icon

--- CcMdcButton.js
+++ CcMdcButton.js
@@ -5,10 +5,18 @@ class CcMdcButton extends HTMLElement {
 
   connectedCallback() {
     var label = this.getAttribute("label") || "Label";
+    var icon = this.getAttribute("icon") || null;
 
-    this.innerHTML = `<button class="mdc-button mdc-button--raised">
-    <span class="mdc-button__label">${label}</span>
-  </button>`;
+    if (icon) {
+      this.innerHTML = `<button class="mdc-button mdc-button--raised">
+      <i class="material-icons mdc-button__icon" aria-hidden="true">${icon}</i>
+      <span class="mdc-button__label">${label}</span>
+    </button>`;
+    } else {
+      this.innerHTML = `<button class="mdc-button mdc-button--raised">
+      <span class="mdc-button__label">${label}</span>
+    </button>`;
+    }
 
     this.button = this.childNodes[0];
     this.mdcComponent = mdc.ripple.MDCRipple.attachTo(this.button);
