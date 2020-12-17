467fc92f9463a0ea2e92012113886e494b0252e3
Alex Brickwedde <alex@brickwedde.de>
2020-12-17 22:49:22 +0100

    x

--- CcTinyMCEEditor.js
+++ CcTinyMCEEditor.js
@@ -11,7 +11,7 @@ class CcTinyMCEEditor extends HTMLElement {
 
     tinymce.init( {
       target: this.childNodes[0], 
-      plugins: 'autoresize image emoticons',
+      plugins: 'image emoticons',
       setup:(ed) => {
         this.editor = ed;
         ed.on('change', (e) => {
