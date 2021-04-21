5ab149727494b098a6b6656f6cb872aa427583a1
Alexander Brickwedde <alex_git@brickwedde.de>
2021-04-21 11:34:19 +0200

    Bug fix

--- CcMdcListItem.js
+++ CcMdcListItem.js
@@ -165,50 +165,51 @@ class CcMdcListItemCb extends HTMLLIElement {
 
 window.customElements.define("cc-mdc-list-item-cb", CcMdcListItemCb, { extends: "li" });
 
+if (typeof CcDragDrop != "undefined") {
+  class CcMdcListItemDndCb extends CcDragDrop {
+    constructor(fillcb) {
+      super(true);
+      this._inactive = false;
+      this._activated = false;
+      this._connected = false;
+      this.fillcb = fillcb;
+    }
 
-class CcMdcListItemDndCb extends CcDragDrop {
-  constructor(fillcb) {
-    super(true);
-    this._inactive = false;
-    this._activated = false;
-    this._connected = false;
-    this.fillcb = fillcb;
-  }
+    connectedCallback() {
+      this._connected = true;
+      this.redraw();
+    }
 
-  connectedCallback() {
-    this._connected = true;
-    this.redraw();
-  }
+    redraw ()  {
+      this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
+      this.style.cursor = this._inactive ? "default" : "pointer";
+      if (this._connected && this.fillcb) {
+        this.fillcb(this);
+      }
+    }
 
-  redraw ()  {
-    this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
-    this.style.cursor = this._inactive ? "default" : "pointer";
-    if (this._connected && this.fillcb) {
-      this.fillcb(this);
+    disconnectedCallback() {
+      this.fillcb = null;
+    }
+
+    set activated (activated) {
+      this._activated = activated;
+      this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
+      this.style.cursor = this._inactive ? "default" : "pointer";
+    }
+
+    set selected (selected) {
+      this._selected = selected;
+      this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
+      this.style.cursor = this._inactive ? "default" : "pointer";
+    }
+
+    set inactive (inactive) {
+      this._inactive = inactive;
+      this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
+      this.style.cursor = this._inactive ? "default" : "pointer";
     }
   }
 
-  disconnectedCallback() {
-    this.fillcb = null;
-  }
-
-  set activated (activated) {
-    this._activated = activated;
-    this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
-    this.style.cursor = this._inactive ? "default" : "pointer";
-  }
-
-  set selected (selected) {
-    this._selected = selected;
-    this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
-    this.style.cursor = this._inactive ? "default" : "pointer";
-  }
-
-  set inactive (inactive) {
-    this._inactive = inactive;
-    this.className = "mdc-list-item" + (this._activated ? " mdc-list-item--activated" : "") + (this._selected ? " mdc-list-item--selected" : "");
-    this.style.cursor = this._inactive ? "default" : "pointer";
-  }
+  window.customElements.define("cc-mdc-list-item-dnd-cb", CcMdcListItemDndCb);
 }
-
-window.customElements.define("cc-mdc-list-item-dnd-cb", CcMdcListItemDndCb);
