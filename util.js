709d32d240ea32eaa3a238e6282ce7f826c07b22
Alex Brickwedde <alex_git@brickwedde.de>
2021-12-23 10:34:10 +0100

    Added util

---
+++ util.js
@@ -0,0 +1,22 @@
+String.prototype.escapeXml = function escapeXml () {
+  var span = document.createElement("span");
+  span.innerText = this;
+  return span.innerHTML;
+}
+
+const html = function html(strings, ...values) {
+  let str = '';
+  strings.forEach((string, i) => {
+    var s = (values.length > i) ? values[i] : "";
+    if (!isDefined(s) || !s.escapeXml) {
+      if (isDefined(s) && s.toString) {
+        s = s.toString();
+      } else {
+        s = "" + s;
+      }
+    }
+    str += string + s.escapeXml();
+  });
+  return str;
+}
+
